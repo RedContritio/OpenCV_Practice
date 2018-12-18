af945da1194d460c1c92265759bc69e391f201bc
RedContritio <591369735@qq.com>
2018-12-18 22:35:32 +0800

    Update

--- 006 PixerOperation/main.cpp
+++ 006 PixerOperation/main.cpp
@@ -9,7 +9,30 @@
 using std::cout ;
 using std::endl ;
 
+void ColorSpaceReduce( cv::Mat& img, cv::Size sz)
+{
+	for(int i=0 ;i<sz.height ;i++ )
+	{
+		for(int j=0 ;j<sz.width ;j++ )
+		{
+			cv::Vec3b& rgba =img.at<cv::Vec3b>( i, j);
+			rgba[0]^= 0xFF;
+			rgba[1]^= 0xFF;
+			rgba[2]^= 0xFF;
+		}
+	}
+}
+
 int main( void)
 {
+	cv::Mat img = cv::imread( "small.png");
+	cv::Size sz( img.cols, img.rows);
+	printf( "%d %d\n", img.cols, img.rows);
+//	cv::resize( img, img, sz);
+
+	ColorSpaceReduce( img, sz);
+
+	cv::imshow( "1", img);
+	cv::waitKey( 0);
 	return 0;
 }
